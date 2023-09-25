664b7f3bcbe088be2182d8a2fd32593327fd4bca
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-09-24 21:58:04 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,58 +1,55 @@
-import path from 'path'
-import cors from 'cors'
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import userRoutes from './routes/userRoutes.js'
-import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+import path from 'path';
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
 // Deployment configuration
-//configure env file in dev mode
-dotenv.config()
-
-// configure env file in production
+// Configure env file
+dotenv.config();
 if (process.env.NODE_ENV === undefined) {
-  dotenv.config({ path: '../.env' })
+  dotenv.config({ path: '../.env' });
 }
 
 // Connect to database
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 // Body parser
-app.use(express.json())
+app.use(express.json());
 
 // CORS
 app.use(
   cors({
     origin: '*',
   })
-)
+);
 
 // API routes
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
 
+// Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
-
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
+  );
 }
 
 // Middleware
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
       .yellow.bold
   )
-)
+);
